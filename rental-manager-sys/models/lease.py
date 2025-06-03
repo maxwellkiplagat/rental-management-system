3561a1b4f14d28eed355b80cc19198628e46c22d
maxwellkiplagat <maxwellkiplagat0@gmail.com>
2025-06-03 23:12:22 +0300

    made some minor changes

--- rental-manager-sys/models/lease.py
+++ rental-manager-sys/models/lease.py
@@ -7,14 +7,22 @@ class Lease(Base):
     __tablename__ = 'leases'
     id = Column(Integer, primary_key=True)
     tenant_id = Column(Integer, ForeignKey('tenants.id'))
+    room_id = Column(Integer, ForeignKey('rooms.id'))
     property_id = Column(Integer, ForeignKey('properties.id'))
     start_date = Column(Date, default=datetime.date.today)
     duration_months = Column(Integer, nullable=False)
 
     tenant = relationship("Tenant", back_populates="leases")
     property = relationship("Property", back_populates="leases")
+    room = relationship("Room", back_populates="lease")
     payments = relationship("Payment", back_populates="lease")
 
 
     def __repr__(self):
-        return f"Lease(id={self.id}, tenant={self.tenant.name}, property={self.property.address},start_date={self.start_date}, duration_months={self.duration_months})"
+        return (f"Lease(id={self.id}, "
+            f"tenant={self.tenant.name if self.tenant else 'N/A'}, "
+            f"property={self.property.address if self.property else 'N/A'}, "
+            f"room={self.room.room_number if self.room else 'N/A'}, "
+            f"start_date={self.start_date}, "
+            f"duration_months={self.duration_months})"
+            )
